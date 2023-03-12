dc1cffccdf357b65205d9fa1ed86cde4d6062f05
kiny123 <89192734912nik@gmail.com>
2023-03-13 02:19:33 +0300

    day 76

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var distanceReading: UILabel!
     var locationManager: CLLocationManager?
 
+    @IBOutlet var circle: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager?.requestAlwaysAuthorization()
 
         view.backgroundColor = .gray
+        
+        circle.layer.cornerRadius = 126
         
     }
     
@@ -48,29 +51,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             case .unknown:
                 self.view.backgroundColor = UIColor.gray
                 self.distanceReading.text = "UNKNOWN"
-
+                self.circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                
             case .far:
                 self.view.backgroundColor = UIColor.blue
                 self.distanceReading.text = "FAR"
+                self.circle.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
 
             case .near:
                 self.view.backgroundColor = UIColor.orange
                 self.distanceReading.text = "NEAR"
+                self.circle.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
 
             case .immediate:
                 self.view.backgroundColor = UIColor.red
                 self.distanceReading.text = "RIGHT HERE"
+                self.circle.transform = CGAffineTransform(scaleX: 1, y: 1)
                 
             default:
                 self.view.backgroundColor = UIColor.gray
                 self.distanceReading.text = "UNKNOWN"
+                self.circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+
             }
         }
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
             update(distance: beacon.proximity)
+            
+            let ac = UIAlertController(title: "We detected a beacon!", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .cancel))
+            present(ac, animated: true)
         } else {
             update(distance: .unknown)
         }
